7f32452ce4e860bbf895f7e906cb873de3606586
srikanth <srikanthd@novisyn.com>
2019-11-05 13:02:26 +0530

    added json data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -100,7 +101,6 @@ class HomePage extends State<HomeScreen> {
       "4"
     ];
 
-
     Widget menuSection = Container(
       padding: EdgeInsets.all(10.0),
       height: 60.0,
@@ -147,96 +147,110 @@ class HomePage extends State<HomeScreen> {
       child: Center(
           child: Container(
         height: 600.0,
-        child: ListView.builder(
-            itemCount: litems.length,
-            itemBuilder: (BuildContext ctxt, int index) {
-              return Card(
-                child: Container(
-                  height: 120.0,
-                  child: Container(
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                            flex: 1,
-                            child: Container(
-                                height: 90.0,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: NetworkImage(
-                                          'https://assets3.thrillist.com/v1/image/2797371/size/tmg-article_default_mobile.jpg')),
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(15.0)),
-                                ))),
-                        Expanded(
-                            flex: 2,
-                            child: Container(
-                              padding: EdgeInsets.only(left: 10.0, top: 20.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Container(
-                                    child: Text(
-                                      "Fried Chicken ",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16.0),
-                                    ),
-                                  ),
-                                  Container(
-                                    padding: EdgeInsets.only(top: 5),
-                                    child: Text(
-                                      "INR 10.00",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16.0),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            )),
-                        Expanded(
-                            flex: 2,
-                            child: Container(
-                              height: 40,
-                              child: Card(
-                                color: Colors.orange[800],
-                                child: Row(
-                                  children: <Widget>[
-                                    Expanded(
-                                      flex: 1,
-                                      child: FlatButton(
-                                          child: Icon(Icons.remove),
-                                          onPressed: (subtractNumbers)),
-                                    ),
-                                    Expanded(
-                                        flex: 1,
-                                        child: Center(
+        child: Container(
+          child: Center(
+            child: FutureBuilder(
+              builder: (context, snapshot) {
+                var myData = json.decode(snapshot.data.toString());
+                return new ListView.builder(
+                  itemCount: myData == null ? 0 : myData.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      child: Container(
+                        height: 120.0,
+                        child: Container(
+                          child: Row(
+                            children: <Widget>[
+                              Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                      height: 90.0,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            fit: BoxFit.cover,
+                                            image: NetworkImage(
+                                                myData[index]["image"])),
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(15.0)),
+                                      ))),
+                              Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                    padding:
+                                        EdgeInsets.only(left: 10.0, top: 20.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Container(
                                           child: Text(
-                                            '$number',
-                                            style: new TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 14.0,
-                                              fontFamily: 'Roboto',
-                                            ),
+                                            myData[index]["name"],
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16.0),
                                           ),
-                                        )),
-                                    Expanded(
-                                      flex: 1,
-                                      child: FlatButton(
-                                          child: Icon(Icons.add),
-                                          onPressed: (addNumbers)),
+                                        ),
+                                        Container(
+                                          padding: EdgeInsets.only(top: 5),
+                                          child: Text(
+                                            "INR " + myData[index]["price"],
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16.0),
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                  ],
-                                ),
-                              ),
-                            )),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            }),
+                                  )),
+                              Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                    height: 40,
+                                    child: Card(
+                                      color: Colors.orange[800],
+                                      child: Row(
+                                        children: <Widget>[
+                                          Expanded(
+                                            flex: 1,
+                                            child: FlatButton(
+                                                child: Icon(Icons.remove),
+                                                onPressed: (subtractNumbers)),
+                                          ),
+                                          Expanded(
+                                              flex: 1,
+                                              child: Center(
+                                                child: Text(
+                                                  '$number',
+                                                  style: new TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 14.0,
+                                                    fontFamily: 'Roboto',
+                                                  ),
+                                                ),
+                                              )),
+                                          Expanded(
+                                            flex: 1,
+                                            child: FlatButton(
+                                                child: Icon(Icons.add),
+                                                onPressed: (addNumbers)),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  )),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              future: DefaultAssetBundle.of(context)
+                  .loadString("assets/person.json"),
+            ),
+          ),
+        ),
       )),
     );
 
