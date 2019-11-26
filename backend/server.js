d0425fa162e59bf01fa73dc61a5cc1e5b5b20e9f
narendrachary93 <narendrachary93@gmail.com>
2019-11-26 21:25:45 +0530

    started

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+// create express app
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 // Configuring the database
 const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
@@ -17,25 +27,17 @@ mongoose.connect(dbConfig.url, {
     process.exit();
 });
 
-
-// create express app
-const app = express();
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
-
 // define a simple route
 app.get('/', (req, res) => {
     res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
 });
 
-// Require Notes routes
-require('./app/routes/user.routes.js')(app);
+require('./app/routes/note.routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port 3000");
 });
+
+// var port_number = server.listen(process.env.PORT || 3000);
+// app.listen(port_number);
